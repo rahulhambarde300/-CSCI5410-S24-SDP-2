54369cd05fe615dd315ba81872410219100eb592
rahul <rh377162@dal.ca>
2024-06-29 19:36:04 -0300

    feat: Lambda functions completed for registration and login notifications

---
+++ Backend/notifications/sendRegistrationNotification.js
@@ -0,0 +1,87 @@
+import { SNSClient, PublishCommand, SubscribeCommand  } from "@aws-sdk/client-sns";
+
+const sns = new SNSClient({ region: "us-east-1" });
+
+const topicArn = 'arn:aws:sns:us-east-1:960148008907:Registration';
+const loginTopicArn = 'arn:aws:sns:us-east-1:960148008907:Login';
+
+export const handler = async(event) => {
+    const {userId, userName, email} = JSON.parse(event.body);
+
+    if(!userId || !userName || !email){
+        return {
+            statusCode: 400,
+            body: JSON.stringify({message: "Please pass all the fields"})
+        };
+    }
+    const filterPolicy = {
+        "UserId": [userId]
+      };
+
+    const registrationSubscribeParams = {
+        TopicArn: topicArn,
+        Protocol: 'email',
+        Endpoint: email,
+        Attributes: {
+          'FilterPolicy': JSON.stringify(filterPolicy)
+        }
+    };
+
+    const loginSubscribeParams = {
+        TopicArn: loginTopicArn,
+        Protocol: 'email',
+        Endpoint: email,
+        Attributes: {
+          'FilterPolicy': JSON.stringify(filterPolicy)
+        }
+    };
+
+    const subject = "Welcome to DalVacationHome!";
+    const message = `Hi ${userName},
+    
+    We're excited to have you join us at DalVacationHome!
+    Your registration was successful, and you’re now part of our community. Here’s what you can do next:
+    - Explore Our Home: Discover our available rooms.
+    - Booking & Queries: Choose your rooms and book it.
+    - Virtual Chatbot Support: Our chatbot can assist with queries regarding your booking.
+    - Get Support: Our team is here to assist you whenever you need help.
+
+    Need help getting started? Reach out to our support team.
+    Thank you for joining us. We’re thrilled to have you!
+    `
+
+    try{
+        const registrationSubscribeCommand = new SubscribeCommand(registrationSubscribeParams);
+        const loginSubscribeCommand = new SubscribeCommand(loginSubscribeParams);
+        //TODO: Uncomment this if you need to subscribe new emails for registration from code
+        //await sns.send(subscribeCommand);
+        await sns.send(loginSubscribeCommand);
+
+        await sns.send(new PublishCommand({
+            Message: message,
+            Subject: subject,
+            MessageAttributes: {
+                UserId: {
+                    DataType: 'String',
+                    StringValue: userId,
+                    },
+                },
+            TopicArn: topicArn
+          }));
+      
+
+        return {
+            statusCode: 201,
+            body: JSON.stringify({message: "Email sent successfully!", details: message})
+        };
+    }
+    catch(error) {
+        console.error(error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({message: "Failed to send email."})
+        };
+    }
+    
+
+};
--- src/components/booking/RoomBooking.js
+++ src/components/booking/RoomBooking.js
@@ -1,10 +1,50 @@
 import React, { useState } from 'react';
-import { Container, Typography, TextField, Button, Select, MenuItem, FormControl, InputLabel, Grid, Paper
+import { Container, Typography, TextField, Button, Select, MenuItem, FormControl, InputLabel, Grid, Paper, TableContainer
 } from '@mui/material';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import { SnackbarProvider, useSnackbar } from 'notistack';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import { styled } from '@mui/material/styles';
+import DeleteIcon from '@mui/icons-material/Delete';
+import IconButton from '@mui/material/IconButton';
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  '&:nth-of-type(odd)': {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  '&:last-child td, &:last-child th': {
+    border: 0,
+  },
+}));
+
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
 
 const RoomBooking = () => {
   const [selectedRoom, setSelectedRoom] = useState('');
@@ -13,7 +53,7 @@ const RoomBooking = () => {
   const [bookings, setBookings] = useState([]);
   const { enqueueSnackbar } = useSnackbar();
 
-  const rooms = ['Room 101', 'Room 102', 'Room 103'];
+  const rooms = ['101', '102', '103'];
 
   //Show alert messages
   const showMessage = (msg, variant) => {
@@ -43,7 +83,7 @@ const RoomBooking = () => {
   };
 
   return (
-    <SnackbarProvider maxSnack={3}>
+    <>
     <Container maxWidth="sm">
       <Typography variant="h4" gutterBottom>
         Book a Room
@@ -93,7 +133,47 @@ const RoomBooking = () => {
         </Grid>
       </Paper>
     </Container>
-    </SnackbarProvider>
+    <Container maxWidth="sm" sx={{ pt: 2 }}>
+      <Typography variant="h4" gutterBottom>
+        Your bookings
+      </Typography>
+      <Paper >
+      <Table sx={{ minWidth: 500 }}  size="small" aria-label="Bookings table">
+        <TableHead>
+          <TableRow sx={{backgroundColor:'#FAFAFA', fontSize: 14}}>
+            <TableCell>Room</TableCell>
+            <TableCell align="right">Start Date</TableCell>
+            <TableCell align="right">End Date</TableCell>
+            <TableCell align="right"></TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.bookingId}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {/* {row.roomNumber} */}301
+              </TableCell>
+              <TableCell align="right">
+                {/* {row.startDate} */}2024-06-29
+              </TableCell>
+              <TableCell align="right">
+                {/* {row.endDate} */}2024-07-04
+              </TableCell>
+              <TableCell align="right">
+                <IconButton color="error">
+                  <DeleteIcon />
+                </IconButton>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+      </Paper>
+    </Container>
+    </>
   );
 };
 
