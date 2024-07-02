651878cdfa3b7bd9f76388cf19d895d9d25b2cec
Mann <mn906219@dal.ca>
2024-07-02 15:41:13 -0300

    Added the Home Page and the Header Component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Header } from './components/Header';
 import RoomBookingNotistack from './components/booking/RoomBooking';
+import Home from './components/home/Home.jsx';
+import Chatbot from './components/chatbot/ChatBot';
 
 function App() {
   return (
-    <div className="App">
-      React app
-      <RoomBookingNotistack/>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+        <Route path="/" element={<Home />} />
+          <Route path="/booking" element={<RoomBookingNotistack />} />
+          <Route path="/chatbot" element={<Chatbot />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
